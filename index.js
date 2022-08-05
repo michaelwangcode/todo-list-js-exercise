424edf8bf992fbe624186b2473f09b2ffd41f16a
Michael Wang <michaelwangxv@gmail.com>
2022-08-05 18:26:29 +0000

    First commit

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Create new task
+function newTask(title, description) {
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+  // Create a task object
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    // Prints out the provided task's details
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // Marks the provided task as completed
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logState();       // Clean Cat Litter has not been completed
+task1.markCompleted();  // Mark task as completed
+console.log("Marked task as completed");
+task1.logState();       // Clean Cat Litter has been completed
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
